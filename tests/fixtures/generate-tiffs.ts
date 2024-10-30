689c854814f781e1a341c7a91a6b351ba06e552e
Omar Siam <omar.siam@oeaw.ac.at>
2024-10-30 23:24:56 +0100

    Use node bindings for vips (sharp) to generate tiffs much faster

---
+++ tests/fixtures/generate-tiffs.ts
@@ -0,0 +1,40 @@
+import sharp from "sharp"
+
+async function main() {
+    let background = sharp({
+        create: {
+            width: 1920,
+            height: 1080,
+            channels: 3,
+            background: { r: 255, g: 255, b: 255 }
+        }
+    }
+    )
+        .toColourspace('rgb16')
+        .tiff({
+            compression: 'lzw'
+        })
+    for (let i = 1; i <= 100; i++) {
+        let text = await sharp({
+            text: {
+                text: i.toString(),
+                font: "NotoSans 36",
+                dpi: 600
+            }
+        })
+            .negate()
+            .tiff()
+            .toBuffer()
+
+        await background.clone()
+            .composite([{
+                input: text,
+                top: 200 + i * 20 % 200,
+                left: 200 + i * 20 % 200
+            }])
+            .linear([1, 1, 1], [255, 0, 0])
+            .toFile('test_' + i + '.tiff')
+    }
+}
+
+main()
