f2068419b05b37acd00674d45fd524c952dfa7e9
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-10-31 12:51:48 +0100

    Parameterize this module

--- tests/fixtures/generate-tiffs.mjs
+++ tests/fixtures/generate-tiffs.mjs
@@ -1,10 +1,25 @@
 import sharp from "sharp"
 
-async function main() {
+/**
+ * Creates white tiffs with red numbers in the specified path.
+ *
+ * @param {string} base_path - Output path
+ * @param {number} count - Number of times
+ * @param {number} width - image width
+ * @param {number} height - image height
+ * @param {string} prefix - Prefix of the file name
+ * @param {string} extension - File extension (only .tif and .tiff are supported)
+ * @param {string} font - Font and Font Size
+ * @param {number} top - initial top position of the number
+ * @param {number} left - initial left position of the number
+ * @param {number} offset - offset added to the position of the number times the number
+ */
+
+export default async function generate_images(base_path, count = 100, width = 1920, height = 1080, prefix = "test_", extension = ".tiff", font = "NotoSans 36", top = 200, left = 200, offset = 10) {
     let background = sharp({
         create: {
-            width: 1920,
-            height: 1080,
+            width,
+            height,
             channels: 3,
             background: { r: 255, g: 255, b: 255 }
         }
@@ -14,11 +29,11 @@ async function main() {
         .tiff({
             compression: 'lzw'
         })
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= count; i++) {
         let text = await sharp({
             text: {
                 text: i.toString(),
-                font: "NotoSans 36",
+                font,
                 dpi: 600
             }
         })
@@ -29,12 +44,12 @@ async function main() {
         await background.clone()
             .composite([{
                 input: text,
-                top: 200 + i * 20 % 200,
-                left: 200 + i * 20 % 200
+                top: top + i * offset % top,
+                left: left + i * offset % left
             }])
             .linear([1, 1, 1], [255, 0, 0])
-            .toFile(import.meta.dirname + '/test_' + i + '.tiff')
+            .toFile(base_path + '/' + prefix + i + extension)
     }
 }
 
-main()
+generate_images(import.meta.dirname)
